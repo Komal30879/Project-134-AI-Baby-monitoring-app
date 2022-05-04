baa0537ee035a1a334625a22a70347d38712bc03
Komal30879 <82501759+Komal30879@users.noreply.github.com>
2022-05-04 18:56:00 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,9 +1,7 @@
-img = "";
 status = "";
-objects = [];
 
 function preload(){
-    song = loadSound("alarm_r.mp3");
+    
 }
 
 function setup(){
@@ -21,43 +19,7 @@ function model_loaded(){
     status = true;
 }
 
-function got_result(error, results){
-    if(error){
-        console.error(error);
-    }
-    console.log(results);
-}
 
 function draw(){
     image(video, 0,0, 380, 380);
-    if(status != ""){
-        r = random(255);
-        g = random(255);
-        b = random(255);
-
-        objectDetector.detect(video,got_result);
-
-        for(i=0; i<objects.length; i++){
-            document.getElementById("status").innerHTML = "Status: Object Detected";
-            fill(r,g,b);
-            percent = floor(objects[i].confidence*100);
-            text(objects[i].label+ "" + percent + "%", objects[i].x + 15, objects[i].y + 15);
-            noFill();
-            stroke(r,g,b);
-            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
-
-            if(objects[i].label == "person"){
-                document.getElementById("baby_status").innerHTML = "Baby Detected";
-                song.stop();
-            } else {
-                document.getElementById("baby_status").innerHTML = "Baby not detected";
-                song.play();
-            }
-        }
-
-        if(objects[i].length<0){
-            document.getElementById("baby_status").innerHTML = "Baby not detected";
-            song.play();
-        }
-    }
 }
